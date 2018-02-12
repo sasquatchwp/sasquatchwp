74fbfb0a171e655f4610bb02e00d586c401e6da3
Maciej Palmowski <m.palmowski@spiders.agency>
2018-02-12 23:47:28 +0100

    Update production.php

--- config/environments/production.php
+++ config/environments/production.php
@@ -5,3 +5,7 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 /** Disable all file modifications including updates and update notifications */
 define('DISALLOW_FILE_MODS', true);
+// autosave
+define('AUTOSAVE_INTERVAL', 300); // seconds
+// post revisions
+define('WP_POST_REVISIONS', 3);
