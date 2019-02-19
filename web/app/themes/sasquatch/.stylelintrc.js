23d986799a64626c54c285c1849caa91a441037b
Maciej Palmowski <m.palmowski@spiders.agency>
2019-02-19 22:41:35 +0100

    wip - new builder

---
+++ web/app/themes/sasquatch/.stylelintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  'rules': {
+    'no-empty-source': null,
+    'at-rule-no-unknown': [
+      true,
+      {
+        'ignoreAtRules': [
+          'extend',
+          'at-root',
+          'debug',
+          'warn',
+          'error',
+          'if',
+          'else',
+          'for',
+          'each',
+          'while',
+          'mixin',
+          'include',
+          'content',
+          'return',
+          'function',
+          'tailwind',
+          'apply',
+          'responsive',
+          'variants',
+          'screen',
+        ],
+      },
+    ],
+  },
+};
